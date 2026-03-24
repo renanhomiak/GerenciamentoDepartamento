289d452721e9e0868e2ab6fcf5986bc3bf238abf
renanhomiak <renanhomiakg@gmail.com>
2026-03-24 15:42:57 -0300

    segundo commit

---
+++ src/main/java/com/homiak/ex08/repository/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.homiak.ex08.repository;
+
+import com.homiak.ex08.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}
